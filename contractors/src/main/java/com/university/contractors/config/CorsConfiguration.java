d8cebdfecf7fe02f31f1f8a9e9d59ab2d9621f95
Oleg Barmin <olegbarmin@gmail.com>
2019-04-14 21:29:01 +0300

    Fix CORS configuration.

--- contractors/src/main/java/com/university/contractors/config/CorsConfiguration.java
+++ contractors/src/main/java/com/university/contractors/config/CorsConfiguration.java
@@ -1,20 +1,15 @@
 package com.university.contractors.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfiguration {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
+@Component
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
