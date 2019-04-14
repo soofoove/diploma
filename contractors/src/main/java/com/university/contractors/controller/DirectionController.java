994f479abb30196a18870b5beb766960e5f858da
Oleg Barmin <olegbarmin@gmail.com>
2019-04-14 14:58:47 +0300

    Add search of Directions by 'faculty_id'.

--- contractors/src/main/java/com/university/contractors/controller/DirectionController.java
+++ contractors/src/main/java/com/university/contractors/controller/DirectionController.java
@@ -2,14 +2,24 @@ package com.university.contractors.controller;
 
 import com.university.contractors.config.Endpoints;
 import com.university.contractors.model.Direction;
-import org.springframework.data.repository.CrudRepository;
+import com.university.contractors.repository.DirectionRepository;
+import com.university.contractors.repository.FacultyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class DirectionController extends AbstractCrudControllerBase<Long, Direction> {
 
-    public DirectionController(CrudRepository<Direction, Long> crudRepository) {
-        super(crudRepository);
+    private final DirectionRepository directionRepository;
+    private final FacultyRepository facultyRepository;
+
+    @Autowired
+    public DirectionController(DirectionRepository directionRepository, FacultyRepository facultyRepository) {
+        super(directionRepository);
+        this.directionRepository = directionRepository;
+        this.facultyRepository = facultyRepository;
     }
 
     @Override
@@ -22,6 +32,15 @@ public class DirectionController extends AbstractCrudControllerBase<Long, Direct
     @GetMapping(path = Endpoints.DIRECTIONS)
     public Iterable<Direction> getAll() {
         return super.getAll();
+    }
+
+    @GetMapping(path = Endpoints.DIRECTIONS, params = {"faculty_id"})
+    public Iterable<Direction> getDirectionByFacultyId(@RequestParam(name = "faculty_id") Long facultyId) {
+        if (facultyRepository.existsById(facultyId)) {
+            return directionRepository.findByFacultyId(facultyId);
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Faculty with ID '%s' was not found", facultyId));
     }
 
     @Override
