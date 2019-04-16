d946527d94c9de4cefc53679e7132e42b89aa1c3
Mykhailo Ryzhov <mykhailo.ryzhov@stryker.com>
2019-04-16 12:28:59 +0300

    Add student search

---
+++ contractors/contractors-server/src/main/java/com/university/contractors/controller/EducationLanguageController.java
@@ -0,0 +1,20 @@
+package com.university.contractors.controller;
+
+import com.university.contractors.config.Endpoints;
+import com.university.contractors.model.EducationLanguage;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EducationLanguageController extends AbstractCrudControllerBase<Long, EducationLanguage> {
+    public EducationLanguageController(CrudRepository<EducationLanguage, Long> crudRepository) {
+        super(crudRepository);
+    }
+
+    @Override
+    @GetMapping(path = Endpoints.EDUCATION_LANGUAGE)
+    public Iterable<EducationLanguage> getAll() {
+        return super.getAll();
+    }
+}
