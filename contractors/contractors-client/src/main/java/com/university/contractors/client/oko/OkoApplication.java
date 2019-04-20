62c29f926ec079f42b90a55dace5f76fec6a6dc7
Oleg Barmin <obarmin@cisco.com>
2019-04-20 18:28:17 +0300

    Stash

---
+++ contractors/contractors-client/src/main/java/com/university/contractors/client/oko/OkoApplication.java
@@ -0,0 +1,12 @@
+package com.university.contractors.client.oko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkoApplication.class, args);
+    }
+}
