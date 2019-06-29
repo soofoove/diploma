cfdfbbad2ea4fceba100287687bd7472a154e30b
Mykhailo Ryzhov <mykhailo.ryzhov@stryker.com>
2019-06-29 23:26:56 +0300

    [WIP] Update student-info. Personal info done

--- contractors/smo-client/src/app/interfaces/models/contract.interface.ts
+++ contractors/smo-client/src/app/interfaces/models/contract.interface.ts
@@ -33,6 +33,6 @@ export interface ContractInterface {
     comment: string;
     usualPaymentForm: PaymentFormInterface;
     direction: DirectionInterface;
-    isBudget: boolean;
-    isActive: boolean;
+    budget: boolean;
+    active: boolean;
 }
